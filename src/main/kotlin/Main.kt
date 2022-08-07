5a80b9f0bcc7a7f22e4001f6148029b4918d398b
Artem Shaban <ArtemShaban@gmail.com>
2022-08-07 20:38:10 +0800

    Minor code refactor. Measure duration of script processing.

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -3,60 +3,62 @@ import com.github.kittinunf.fuel.gson.responseObject
 import io.reactivex.Flowable
 import io.reactivex.schedulers.Schedulers
 import java.util.concurrent.atomic.AtomicInteger
+import kotlin.system.measureTimeMillis
 
 fun main(args: Array<String>) {
-    println("Hello World!")
+    val elapsed = measureTimeMillis {
 
-    val resourceAsText = getResourceAsText("tickers.txt")
-//    val resourceAsText = getResourceAsText("etf_tickers.txt")
-    val split = resourceAsText?.split("\n")
-    if (split != null) {
+        val assets = listOf("stock", "etf")
 
-        val counter = AtomicInteger(0)
-        Flowable
-            .fromIterable(split)
-            .parallel(1)
-            .runOn(Schedulers.io())
-            .doOnNext { ticker ->
-                try {
-                    Fuel.get("https://api.nasdaq.com/api/quote/$ticker/dividends?assetclass=stocks")
-//                        Fuel.get("https://api.nasdaq.com/api/quote/$ticker/dividends?assetclass=etf")
-                        .header(
-                            mapOf(
-                                "accept-language" to "en",
-                                "user-agent" to "Chrome/101.0.4951.64 Safari/537.36"
-                            )
-                        )
-                        .responseObject<Response>
-                        { _, _, result ->
+        for (asset in assets) {
+            val resourceAsText = getResourceAsText(asset + "_tickers.txt")
 
-                            val dividentYieldString = result.get().data.yield
-                            if (dividentYieldString != "N/A") {
-                                val dividentYield = dividentYieldString.trim { it == '%' }.toDouble()
-                                if (dividentYield >= 10)
-                                    println("$ticker --> $dividentYield%")
-                            }
+            val split = resourceAsText?.split("\n")
+            if (split != null) {
+                val counter = AtomicInteger(0)
+                Flowable
+                    .fromIterable(split)
+                    .parallel(1)
+                    .runOn(Schedulers.io())
+                    .doOnNext { ticker ->
+                        try {
+                            val assetClass = if (asset == "stock") "stocks" else "etf"
+                            Fuel.get("https://api.nasdaq.com/api/quote/$ticker/dividends?assetclass=$assetClass")
+                                .header(
+                                    mapOf(
+                                        "accept-language" to "en",
+                                        "user-agent" to "Chrome/101.0.4951.64 Safari/537.36"
+                                    )
+                                )
+                                .responseObject<Response>
+                                { _, _, result ->
+
+                                    val dividentYieldString = result.get().data.yield
+                                    if (dividentYieldString != "N/A") {
+                                        val dividentYield = dividentYieldString.trim { it == '%' }.toDouble()
+                                        if (dividentYield >= 10)
+                                            println("$ticker --> $dividentYield%")
+                                    }
+                                }
+                                .get()
+
+                        } catch (e: Exception) {
+                            println("$ticker: error on http request")
                         }
-                        .get()
 
-                } catch (e: Exception) {
-//                        println(ticker + ":" +e.localizedMessage)
-                    println(ticker + ": error on http request")
-                }
-
-                val count = counter.incrementAndGet()
-                if (count % 100 == 0) {
-                    println("$count tickers handled")
-                }
+                        val count = counter.incrementAndGet()
+                        if (count % 100 == 0) {
+                            println("$count tickers handled")
+                        }
+                    }
+                    .sequential()
+                    .ignoreElements()
+                    .blockingAwait()
             }
-            .sequential()
-            .ignoreElements()
-            .blockingAwait()
-
+        }
     }
 
-
-    println("Finished")
+    println("Finished. Time spend = " + elapsed / 1000 + "s")
 }
 
 fun getResourceAsText(path: String): String? =
